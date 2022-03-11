8c2aa4f75d76d3c35134c736b59ab385121218aa
TC Chen <zerox12345@hotmail.com>
2022-03-12 01:46:10 +0800

    Another easy version

--- password.py
+++ password.py
@@ -2,14 +2,14 @@ password = 'a123456'
 i = 0
 while i < 3:
 	user = input('請輸入密碼: ')
+	i = i + 1
 	if password == user:
 		print('你猜對惹')
 		break
 	else:
-			if i < 2:
-				print('你猜錯惹 你還剩下', 2 - i, '次機會')
-				i = i + 1
-			else:
-				break
-if i == 2:
+		if i < 3:
+			print('你猜錯惹 你還剩下', 3 - i, '次機會')
+
+			
+if i == 3:
 	print('你沒救惹88')
